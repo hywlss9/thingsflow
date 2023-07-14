216bbc00ff260dad603d3953be850ec6e9d48588
HyeongJin Park <37681882+hywlss9@users.noreply.github.com>
2023-07-14 18:23:46 +0900

    FEAT: react project init

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+
+import List from "./pages/list";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(<List />);
