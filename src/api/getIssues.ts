06b1ae28c68e53df9b11c7d928dbe60a2084de01
HyeongJin Park <37681882+hywlss9@users.noreply.github.com>
2023-07-14 19:04:10 +0900

    FEAT: issue 리스트 api 추가

---
+++ src/api/getIssues.ts
@@ -0,0 +1,87 @@
+import axios from "axios";
+
+export default async function getIssues(): Promise<IssuesData | false> {
+  try {
+    const { data } = await axios.get(
+      `https://api.github.com/repos/angular/angular-cli/issues`,
+      {
+        params: {
+          per_page: 10,
+        },
+      }
+    );
+    console.log({ data });
+    return data;
+  } catch {
+    return false;
+  }
+}
+
+interface IssuesData {
+  active_lock_reason: null;
+  assignee: null;
+  assignees: any[];
+  author_association: string;
+  body: string;
+  closed_at: null;
+  comments: number;
+  comments_url: string;
+  created_at: string;
+  events_url: string;
+  html_url: string;
+  id: number;
+  labels: {
+    color: string;
+    default: boolean;
+    description: string;
+    id: number;
+    name: string;
+    node_id: string;
+    url: string;
+  }[];
+  labels_url: string;
+  locked: boolean;
+  milestone: null;
+  node_id: string;
+  number: number;
+  performed_via_github_app: null;
+  reactions: {
+    "+1": number;
+    "-1": number;
+    confused: number;
+    eyes: number;
+    heart: number;
+    hooray: number;
+    laugh: number;
+    rocket: number;
+    total_count: number;
+    url: string;
+  };
+  repository_url: string;
+  state: string;
+  state_reason: null;
+  timeline_url: string;
+  title: string;
+  updated_at: string;
+  url: string;
+  user: {
+    avatar_url: string;
+    events_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    gravatar_id: string;
+    html_url: string;
+    id: number;
+    login: string;
+    node_id: string;
+    organizations_url: string;
+    received_events_url: string;
+    repos_url: string;
+    site_admin: boolean;
+    starred_url: string;
+    subscriptions_url: string;
+    type: string;
+    url: string;
+  };
+}
