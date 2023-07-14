712060d0e54bd0a2168214cb466f680d00f03860
HyeongJin Park <37681882+hywlss9@users.noreply.github.com>
2023-07-14 18:52:48 +0900

    FEAT: reset css 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import ReactDOM from "react-dom/client";
 
 import Router from "./pages/Router";
 
+import "./reset.css";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
